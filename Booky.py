8b7d66064f8d3b7ceb72b9a8dce76f9447d9a533
Esote <esote@users.noreply.github.com>
2017-04-29 12:10:28 -0500

    Update Booky.py

--- Booky.py
+++ Booky.py
@@ -6,7 +6,7 @@ URL = "https://www.goodreads.com/book/random"
 while True:
 	html_text = requests.get(URL).text
 
-	#	Rating Count
+	# Rating Count
 	bg_rating_count 	= html_text.find("<span class=\"value-title\" title=\"") + 33
 	end_rating_count	= html_text.find("\" itemprop=\"ratingCount\">", bg_rating_count)
 	rating_count 		= int(html_text[bg_rating_count : end_rating_count].replace(',', ''))
@@ -14,18 +14,18 @@ while True:
 	if rating_count >= 30:
 		if "<span itemprop=\"numberOfPages\">" in html_text:
 			
-			#	Title
+			# Title
 			bg_title	= html_text.find("<meta property=\"og:title\" content=\"") + 35
 			end_title	= html_text.find("\"/>", bg_title)
 			title = html_text[bg_title : end_title].replace("&amp;", '&')
 			title = re.sub(r'\((.*)\)','', title)
 
-			#	Pages
+			# Pages
 			bg_pages	= html_text.find("<span itemprop=\"numberOfPages\">") + 31
 			end_pages	= html_text.find(" page", bg_pages)
 			pages 		= int(html_text[bg_pages : end_pages])
 
-			#	Rating
+			# Rating
 			bg_rating	= html_text.find("<span class=\"average\" itemprop=\"ratingValue\">") + 45
 			end_rating	= html_text.find("</", bg_rating)
 			rating		= float(html_text[bg_rating : end_rating])
